a22d14a45670725217d2190bb38f894caa7fcb61
rishal <rishalcheran@gmail.com>
2024-01-18 23:39:28 +0530

    first commit

---
+++ ems-backendd/src/main/java/net/javaguides/emsbackendd/EmsBackenddApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.emsbackendd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackenddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBackenddApplication.class, args);
+	}
+
+}
---
+++ ems-backendd/src/test/java/net/javaguides/emsbackendd/EmsBackenddApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.emsbackendd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackenddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
